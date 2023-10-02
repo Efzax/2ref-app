ce45067fbf257aa1b415b1ad734429d421d12371
Efzax <143233938+Efzax@users.noreply.github.com>
2023-10-02 14:15:06 -0300

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -31,6 +31,15 @@ export default function RootLayout({
 	return (
 		<html lang="en" suppressHydrationWarning>
 			<head />
+			<!-- Google tag (gtag.js) -->
+<script async src="https://www.googletagmanager.com/gtag/js?id=G-2NLEBDCR03"></script>
+<script>
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', 'G-2NLEBDCR03');
+</script>
 
 			<body
 				className={clsx(
